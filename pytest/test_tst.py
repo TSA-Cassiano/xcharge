ed2aab9eacb8f78ead8edce59d59e23589b86a9b
TSA-Cassiano <tiagofis96@gmail.com>
2022-12-04 15:12:28 -0300

    try2

--- pytest/test_tst.py
+++ pytest/test_tst.py
@@ -10,8 +10,22 @@ import sys
 
 ##################################
 
-print(os.getcwd())
+print('working dir', os.getcwd())
 
+def run_job(config_test,log_name):
+	lab = []
+	for test in config_test:
+		label = '_'.join([str(x) for x in test])
+		param = ' '.join([str(x) for x in test])
+		name  = 'Simulation_'+log_name.split('_')[-1].split('.')[0]+'_'+label+'.txt'
+		lab.append(name)
+		command = "kmc "+log_name+' '+label+' '+param
+		subprocess.run(command, shell=True)
+	return [(lab)]
+	
+simuls = run_job([[1000,10]],"input_test1.py")
 
+txt_files = [i for i in os.listdir('.') if (os.path.isfile(i)) and ('.txt' in i.lower())]
+print('txt files',txt_files)	
 def test_1():
 	assert 2==2
